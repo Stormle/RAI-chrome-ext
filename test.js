74286d75ec23c666b96a74bf6b6b7b7b473e9cbf
Stormle <alvar.stara@gmail.com>
2021-08-17 18:01:27 +0300

    New stuff. Remember to fix backwardsvariable symbol placement.

--- test.js
+++ test.js
@@ -2,29 +2,31 @@ var testtext = "oh welcome, welcome my friend"
 console.log(testtext.indexOf("welcome"))
 
 
-const text = ["We have found 2352.1 gazillionn$USDffrom a vault under the mountain."]
-
+const text2 = ["We have found 2352.1 gazillionn$USDffrom a vault under the mountain."]
+const text = ["We have found $2999 from a vault under the mountain."]
+var modifiedHTML = ""
 for (let i = 0; i < text.length; i++) {
     if (text[i].toLowerCase().indexOf("$usd") != -1) {
-        replaceText(text[i], "$usd", text[i].toLowerCase().indexOf("$usd"))
+        text[i] = replaceText(text[i], "$usd", text[i].toLowerCase().indexOf("$usd"))
     } else if (text[i].indexOf("$") != -1) { 
-        replaceText(text[i].toLowerCase(), "$", text[i].toLowerCase().indexOf("$"))
+        text[i] = replaceText(text[i].toLowerCase(), "$", text[i].toLowerCase().indexOf("$"))
     } else if (text[i].indexOf("US Dollar") != -1) {
-        replaceText(text[i].toLowerCase(), "US Dollar", text[i].toLowerCase().indexOf("US Dollar"))
+        text[i] = replaceText(text[i].toLowerCase(), "US Dollar", text[i].toLowerCase().indexOf("US Dollar"))
     } else if (text[i].indexOf("usd") != -1) {
-        replaceText(text[i].toLowerCase(), "usd", text[i].toLowerCase().indexOf("usd"))
+        text[i] = replaceText(text[i].toLowerCase(), "usd", text[i].toLowerCase().indexOf("usd"))
     } else if (text[i].indexOf("dollars") != -1) {
-        replaceText(text[i].toLowerCase(), "dollars", text[i].toLowerCase().indexOf("dollars"))
+        text[i] = replaceText(text[i].toLowerCase(), "dollars", text[i].toLowerCase().indexOf("dollars"))
     } else if (text[i].indexOf("dollar") != -1) {
-        replaceText(text[i].toLowerCase(), "dollar", text[i].toLowerCase().indexOf("dollar"))
+        text[i] = replaceText(text[i].toLowerCase(), "dollar", text[i].toLowerCase().indexOf("dollar"))
     } else if(text[i].indexOf("¢") != -1){
-        replaceText(text[i].toLowerCase(), "¢", text[i].toLowerCase().indexOf("¢"))
+        text[i] = replaceText(text[i].toLowerCase(), "¢", text[i].toLowerCase().indexOf("¢"))
     } else if(text[i].indexOf("cents") != -1){
-        replaceText(text[i].toLowerCase(), "cents", text[i].toLowerCase().indexOf("cents"))
+        text[i] = replaceText(text[i].toLowerCase(), "cents", text[i].toLowerCase().indexOf("cents"))
     } else if(text[i].indexOf("cent") != -1){
-        replaceText(text[i].toLowerCase(), "cent", text[i].toLowerCase().indexOf("cent"))
+        text[i] = replaceText(text[i].toLowerCase(), "cent", text[i].toLowerCase().indexOf("cent"))
     }
 }
+console.log(text[0])
 
 function replaceText(text, foundString, indexOf) {
     console.log(foundString)
@@ -32,45 +34,153 @@ function replaceText(text, foundString, indexOf) {
     var pageEnd = text.substring(indexOf + foundString.length)
     console.log(pageStart)
     var foundNumber = ""
+    var foundNumberBackwards = ""
     var isFound = false
     var isDone = false
     var i = 0;
+    var isBackwards = false
+    var searchBackwards = false
+    var charIter
+    var foundIndex = 10000
+    var foundIndexBackwards = 10000
     while (!isDone) {
-        var charIter = pageStart.charAt(pageStart.length - i)
+        //Get character
+        if (searchBackwards) {
+            if (pageEnd.length > i) {
+                charIter = pageEnd.charAt(i)
+            } else {
+                isdone = true
+            }
+        } else {
+            if (i >= 0) {
+                charIter = pageStart.charAt(pageStart.length - i)
+            } else {
+                isdone = true
+            }
+            
+        }
         if (charIter == "1" || charIter == "2" || charIter == "3" || charIter == "4" || charIter == "5" || charIter == "6" || charIter == "7" || charIter == "8" || charIter == "9" || charIter == "9" || charIter == "." || charIter == ",") {
             //Adding found number character
-            isFound = true
-            foundNumber += charIter
+            if (!isFound && !searchBackwards) {
+                isFound = true
+                foundIndex = i
+            } else if (searchBackwards && !isFound) {
+                isFound = true
+                foundIndexBackwards = i
+            }
+            if (!searchBackwards) {
+                foundNumber += charIter
+            } else {
+                foundNumberBackwards += charIter
+            }
             i++
         } else if (isFound) {
-            //Number ended, stopping the iterations.
-            isDone = true
+            //Already found valid numbers. Checking if we should go the other way.
+            if (foundString.includes("$") || foundString.toLowerCase() == "usd") {
+                if (!searchBackwards) {
+                    searchBackwards = true
+                    isFound = false
+                    i = 0
+                } else {
+                    isDone = true
+                }
+            } else {
+                isDone = true
+            }
+            
         } else if (i < 20) {
-            //Search max 20 chars back
+            //Search max 20 chars without result
             i++
         } else {
-            isDone = true
+            //None found in 20 characters.
+            if (foundString.includes("$") || foundString.toLowerCase() == "usd") {
+                if (!searchBackwards) {
+                    searchBackwards = true
+                    isFound = false
+                    i = 0
+                } else {
+                    isDone = true
+                }
+            } else {
+                isDone = true
+                return false
+            }
         }
     }
-    if (foundNumber =! "") {
+
+    var sendBackwardsVariable
+    if (foundIndexBackwards < foundIndex) {
+        //Backwards
+        sendBackwardsVariable = true
+    } else if (foundIndex == 10000 && foundIndexBackwards == 10000){
+        //Nothing was found.
+        return
+    } else if (foundIndex == foundIndexBackwards){
+        //Tie, just send front.
+        sendBackwardsVariable = false
+    } else if (foundIndexBackwards > foundIndexBackwards) {
+        sendBackwardsVariable = false
+    }
+
+    if (foundNumber != "" || foundNumberBackwards != "") {
+        var actualContent = false
         if ( foundNumber.includes("1") || foundNumber.includes("2") || foundNumber.includes("3") || foundNumber.includes("4") || foundNumber.includes("5") || foundNumber.includes("6") || foundNumber.includes("7") || foundNumber.includes("8") || foundNumber.includes("9") || foundNumber.includes("0") ) {
-            foundNumber = foundNumber.split("").reverse().join("");
-            var convertedNumber = convertNumber(foundNumber, foundString)
-            return pagestart + convertedNumber.toString() + "RAI" + pageEnd
+            actualContent = true
+        } else if ( foundNumberBackwards.includes("1") || foundNumberBackwards.includes("2") || foundNumberBackwards.includes("3") || foundNumberBackwards.includes("4") || foundNumberBackwards.includes("5") || foundNumberBackwards.includes("6") || foundNumberBackwards.includes("7") || foundNumberBackwards.includes("8") || foundNumberBackwards.includes("9") || foundNumberBackwards.includes("0") ) {
+            actualContent = true
+        }
+        if (actualContent) {
+            if (!sendBackwardsVariable){
+                foundNumber = foundNumber.split("").reverse().join("");
+            }
+            var convertedNumber
+            var numberReplaced
+            var returnString = ""
+            if (sendBackwardsVariable) {
+                convertedNumber = convertNumber(foundNumberBackwards, foundString)
+                numberReplaced = replaceTextFromString(pageEnd, convertedNumber.toString(), foundIndexBackwards, foundNumberBackwards.length, true)
+                
+                returnString = pageStart + addStr(numberReplaced, foundIndexBackwards + length, "RAI")
+            } else {
+                convertedNumber = convertNumber(foundNumber, foundString)
+                numberReplaced = replaceTextFromString(pageStart, convertedNumber.toString(), foundIndex, foundNumber.length, false)
+                returnString = pageStart + "RAI" + numberReplaced
+                console.log(returnString)
+            }
+            return returnString
         }
         
     }
     
 }
+function addStr(str, index, stringToAdd){
+    return str.substring(0, index) + stringToAdd + str.substring(index, str.length);
+}
 
+function replaceTextFromString(input, replacement, index, length, isBackwards) {
+    var start = ""
+    var end = ""
+    if (!isBackwards) {
+        //Right to left
+        start = input.substring(0, input.length - index - length + 1)
+        end = input.substring(input.length - index + 1, input.length)
+    } else {
+        //Left to right
+        start = input.substring(0, index - 1)
+        end = input.substring(index + length, input.length)
+    }
+    
+    var totalString = start + replacement + end
+    return totalString
+}
 function convertNumber(inputNumber, foundString) {
-    var sanitized = ""
+    var sanitized = inputNumber
     var typeOfJunk = ""
     if (inputNumber.charAt(0) == "," || inputNumber.charAt(0) == ".") {
         //Starts with a , or . so we add a zero to fix math
         sanitized = "0"
     }
-    if (inputNumber.includes(".") && foundNumber.includes(",")) {
+    if (inputNumber.includes(".") && inputNumber.includes(",")) {
         sanitized = inputNumber.split(',').join("");
         typeOfJunk = "both"
     } else if (inputNumber.includes(",")) {
