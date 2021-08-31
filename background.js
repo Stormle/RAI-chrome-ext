a5d33c3acc7631424cb73b211ee3be8b0cbfc26b
Stormle <alvar.stara@gmail.com>
2021-08-31 14:27:40 +0300

    Documentation + better test html

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-//Set value at $3 in case the API doesn't work. But it does work.
+//Get coingecko price. If it doesn't work, do nothing.
 fetch('https://api.coingecko.com/api/v3/simple/price?ids=rai&vs_currencies=usd')
   .then(
     function(response) {
@@ -20,9 +20,20 @@ fetch('https://api.coingecko.com/api/v3/simple/price?ids=rai&vs_currencies=usd')
 
 
 function start() {
-    var textnodes = nativeSelector()
+    /*The purpose of this function is to take all child nodes considered as text
+    and run the content through a loop of finding the index of mentions of usd.
+    It then passes the original text, found string and the index of the found text 
+    to the replaceText() function.
+    If replaceText() returns a string, the operation of the function was valid
+    and childnode can be replaced with new content; the content being the return.
+    A fail will result replaceText() to return false. 
 
-    console.log(raiPrice)
+    Iteration over found mentions happens by taking the modified version of the node
+    and always taking the first mention left to right. In case a mention was invalidated
+    by the replaceText() function +1 is added to indexBlock which prevents it from being
+    selected on the next iteration.
+    */
+    var textnodes = nativeSelector()
     var modifiedHTML = ""
     var regexp = [/[$]usd/gi,
     /[$]/gi,
@@ -66,16 +77,11 @@ function start() {
             while (stillFinding) {
                     modifiedHTML = replaceText(textnodes[i].nodeValue, correspondingString[i2], matches[0])
                     if (typeof modifiedHTML === 'string') {
-                        if (modifiedHTML.includes("0.00000000000000000000000000000001")) {
-                            console.log("here")
-                        }
                         textnodes[i].nodeValue = modifiedHTML
                     } else {
                         indexBlock++
                     }
-
                     textToSearch = textnodes[i].nodeValue
-
                 //Redo the index search 
                 matches = []
                 var skipcounter = 0
@@ -96,6 +102,7 @@ function start() {
     }
 }
 function nativeSelector() {
+    //This function selects all child nodes that are classified as text.
     var elements = document.querySelectorAll("body, body *");
     var results = [];
     var child;
@@ -110,6 +117,30 @@ function nativeSelector() {
 
 
 function replaceText(text, foundString, indexOf) {
+    /*This function is responsible for finding the possible numeral amount of dollars
+    in text. The core idea is to start iterating over characters leftward from the found
+    string until encountering numbers decimal points. The maximum reach is 20 characters.
+    This 20 character limit is overriden if numbers are found and they extend beyond it.
+    On certain string such as "$", "usd" and "¢" the special case of left to right search
+    is unlocked. If a numeral value is found on each sides of the mention, both values are
+    then validated to be proper numbers and then compared which is closer to the index of
+    the string. All these operations will be done with the string split in to two parts 
+    which made coding replaceTextFromString() and iterating the strings easier.
+
+    The winning number's side will be selected to be replaced in the original string. The 
+    number first is inverted if it's right to left, then fed to the function convertNumber()
+    along with foundString which returns a string with the numerical amount converted to RAI. 
+    We can then piece this all together by giving the function replaceTextFromString() the
+    winning side's split string, the number to replace it, the index of this number found, 
+    length of the found number and if the string found was right to left. We then piece
+    together the start, end (the winning one of them being modified to contain the new number with
+    the found string removed) and "RAI" or " RAI" depending on if there originally was a space
+    between the found number and string or not. If the number was found left to right then the
+    addStr() function is used to replace text from the string in a bit more custom fashion.
+
+    The end result is the final string which is returned. If something in this process fails
+    or the string is deemed as not valid for replacement then false is returned.
+    */
     console.log(foundString)
     var pageStart = text.substring(0, indexOf)
     var pageEnd = text.substring(indexOf + foundString.length)
@@ -127,13 +158,8 @@ function replaceText(text, foundString, indexOf) {
     while (!isDone) {
         //Get character
         if (searchBackwards) {
-            if (pageEnd.length > i && pageEnd.length > 0) {
+            if (pageEnd.length >= i && pageEnd.length > 0) {
                 charIter = pageEnd.charAt(i)
-                if (charIter == "\\") {
-                    //Element changed or someone used backslash in the text between found str and number. Unlikely but that would interupt the number search.
-                    charIter = ""
-                    isDone = true
-                }
             } else {
                 charIter = ""
                 isDone = true
@@ -141,19 +167,12 @@ function replaceText(text, foundString, indexOf) {
         } else {
             if (pageStart.length > i >= 0 && pageStart.length > 0) {
                 charIter = pageStart.charAt(pageStart.length - i)
-                if (charIter == "\\") {
-                    //Element changed or someone used backslash in the text between found str and number. Unlikely but that would interupt the number search.
-                    charIter = ""
-                    if (foundString.includes("$") || foundString.toLowerCase() == "usd" || foundString == "¢") {
-                        searchBackwards = true
-                    } else {
-                        isDone = true
-                    }
-                }
             } else {
                 charIter = ""
                 if (foundString.includes("$") || foundString.toLowerCase() == "usd" || foundString == "¢") {
                     searchBackwards = true
+                    i = 0
+                    continue
                 } else {
                     isDone = true
                 }
@@ -182,6 +201,7 @@ function replaceText(text, foundString, indexOf) {
                     searchBackwards = true
                     isFound = false
                     i = 0
+                    continue
                 } else {
                     charIter = ""
                     isDone = true
@@ -201,6 +221,7 @@ function replaceText(text, foundString, indexOf) {
                     searchBackwards = true
                     isFound = false
                     i = 0
+                    continue
                 } else {
                     charIter = ""
                     isDone = true
@@ -227,8 +248,6 @@ function replaceText(text, foundString, indexOf) {
         } else {
             return false
         }
-
-        
     } else if (foundIndex == 10000 && foundIndexBackwards == 10000){
         //Nothing was found.
         return false
@@ -274,7 +293,6 @@ function replaceText(text, foundString, indexOf) {
                     return false
                 }
                 numberReplaced = replaceTextFromString(pageEnd, convertedNumber.toString(), foundIndexBackwards, foundNumberBackwards.length, true)
-                
                 returnString = pageStart + addStr(numberReplaced, foundIndexBackwards + convertedNumber.toString().length, " RAI")
             } else {
                 convertedNumber = convertNumber(foundNumber, foundString)
@@ -282,8 +300,12 @@ function replaceText(text, foundString, indexOf) {
                     return false
                 }
                 numberReplaced = replaceTextFromString(pageStart, convertedNumber.toString(), foundIndex, foundNumber.length, false)
-
-                returnString = numberReplaced + "RAI" + pageEnd
+                if (Math.abs(foundIndex - indexOf ) == 1) {
+                    returnString = numberReplaced + " RAI" + pageEnd
+                } else {
+                    returnString = numberReplaced + "RAI" + pageEnd
+                }
+                
             }
             return returnString
         }
@@ -292,10 +314,12 @@ function replaceText(text, foundString, indexOf) {
     
 }
 function addStr(str, index, stringToAdd){
+    //Replaces text from a string in a custom position.
     return str.substring(0, index) + stringToAdd + str.substring(index, str.length);
 }
 
 function replaceTextFromString(input, replacement, index, length, isBackwards) {
+    //Replaces text from a string in a custom position. Also supports inverted indexes.
     var start = ""
     var end = ""
     if (!isBackwards) {
@@ -313,6 +337,15 @@ function replaceTextFromString(input, replacement, index, length, isBackwards) {
 }
 
 function convertNumber(inputNumber, foundString) {
+    /* The purpose of this function is to take a number input,
+    sanitize it, convert it in to RAI, round it to the same amount of 
+    decimals as before +2 or until zero's stop with a maximum of 30 and 
+    lastly add the things we sanitized out previously. 
+    100,000.69 ($) -> 100000.69 ($) -> 33333.56123123 (RAI) -> 33,333.56 (RAI)
+    If the string is about cents, the value of the found number is divided
+    by a hundred to correspond the USD/RAI ratio.
+    If the value either goes too small, is not a number (e.g. 192.168.1.1) then return false.
+    */
     var sanitized = inputNumber
     var typeOfJunk = ""
     if (inputNumber.charAt(0) == "," || inputNumber.charAt(0) == ".") {
@@ -383,6 +416,7 @@ function convertNumber(inputNumber, foundString) {
     return finalNumber
 }
 function commaSeparateNumber(val){
+    //This function adds the commas to a comma separated number.
     while (/(\d+)(\d{3})/.test(val.toString())){
       val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
     }
